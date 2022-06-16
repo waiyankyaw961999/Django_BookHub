793766ec39a5999a8412ee509fe7f9edeae40350
waiyankyaw961999 <64473359+waiyankyaw961999@users.noreply.github.com>
2022-06-16 12:45:39 +0630

    Add UserSerializer to AuthorSerializer

--- authors/serializers.py
+++ authors/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import Author
+from accounts.serializers import UserSerializer
 
 
 class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+
+    user = UserSerializer(read_only=True)
+
     class Meta:
         model = Author
-        fields = ["id", "first_name", "last_name", "bio"]
+        fields = ["id", "user", "first_name", "last_name", "bio"]
--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,6 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -69,7 +68,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
